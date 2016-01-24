5382247e3ecbae1716df4bd6c35092575a51d93c
Nick999389 <nik9993@gmail.com>
2016-01-24 06:11:25 +0000

    modified to build on 4.1.13

--- bsc-slave.h
+++ bsc-slave.h
@@ -1,3 +1,3 @@
 #define I2C_SLAVE       0x0703  /* Use this slave address */
 #define I2C_SLAVE_FORCE 0x0706  /* Use this slave address, even if it*/
-
+#define I2C_CLEAR_FIFOS 0xFF01  /* Clear send and receive FIFOS*/
